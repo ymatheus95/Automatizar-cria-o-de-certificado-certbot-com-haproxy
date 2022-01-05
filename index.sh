286681b89d4c8f5baa90ad8cbd16e6869d973f15
Matheus <32114803+ymatheus95@users.noreply.github.com>
2022-01-05 18:04:32 -0300

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
 #############################################################################
 # Autor: Matheus Santos
 # Data Criacao: 19/12/2021
@@ -6,31 +5,31 @@
 #############################################################################
 
 
-email="linux@seu.dominio.com"
-porta="porta"
+email="linux@ati.pe.gov.br"
+porta="54321"
 path_haproxy="/etc/haproxy/haproxy.cfg"
 path_haproxy_certs="/etc/haproxy/certs"
 
 source funcoes.sh
 
 if  test $1;  then
-	if test -f "$1"; then
-		path_haproxy="$1"
-		path_haproxy_certs="" # Tem que corrigir!
+	if [ -d $1 ]; then
+		path_haproxy="$1/haproxy.cfg"
+		path_haproxy_certs=$"$1/certs"
 	else
-		echo "Atencao! Diretorio ou arquivo nao encontrado $1. O primeiro parementro desse script se refere ao diretorio absoluto onde esta o arquivo do haproxy, use apenas quando necessario."
+		echo_texto "vermelho" "Atencao! Diretorio ou arquivo nao encontrado $1. O primeiro parementro desse script se refere ao diretorio absoluto onde esta o arquivo do haproxy, use apenas quando necessario."
 		exit 0
 	fi
 ## Adicionar condicao para verificar se o arquivo existe
 fi
 
 dominio=""
-echo "--------------------------------------------------"
-echo "|Digite o dominio que deseja gerar  o certificado:|"
-echo "--------------------------------------------------"
+echo "---------------------------------------------------"
+echo "|Gerador de certificado - Haproxy com letsencrypt:|"
+echo "---------------------------------------------------"
 
 # Essa funcao verifica se ha alguma ocorrencia seja backend ou seja acl com o dominio fornecido pelo usuario, caso nao tenha nenhum registro no arquivo do haproxy o script sera interrompido
 
-read dominio;
+read -p "Digite o dominio (teste.gob.br): " dominio;
 
 valida_dominio "$dominio" "$path_haproxy" "$path_haproxy_certs"
